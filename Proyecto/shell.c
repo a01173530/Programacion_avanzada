126d4eeea67ba17b67694af624d3be51cc0b2a49
a01173530 <A01173530@itesm.mx>
2021-05-31 01:34:35 -0500

    Proyecto

--- Proyecto/shell.c
+++ Proyecto/shell.c
@@ -4,20 +4,20 @@
 
 void mini_shell()
 {
-    char command_argv[BUFFER_MAX];
+    char cmd_argv[BUFFER_MAX];
     char dir[PATH_MAX];
 
     prompt();
 
 
-    while(fgets(command_argv,BUFFER_MAX, stdin)){
-        if(command_argv[0] == '\n'){
+    while(fgets(cmd_argv,BUFFER_MAX, stdin)){
+        if(cmd_argv[0] == '\n'){
             prompt();
             continue;
-        }else if(strcmp(command_argv,"quit\n") == 0){
+        }else if(strcmp(cmd_argv,"quit\n") == 0){
             break;
         }
-        pipe_get_cmd(command_argv);
+        pipe_get_cmd(cmd_argv);
         prompt();
     }
     exit(0);
@@ -26,25 +26,25 @@ void mini_shell()
 
 void execute_file(char *filepath){
 
-    FILE *fp = fopen(filepath, "r");
+    FILE *filep = fopen(filepath, "r");
     char buffer[BUFFER_MAX];
 
-    while(fgets(buffer,sizeof(buffer),fp) != NULL){
+    while(fgets(buffer,sizeof(buffer),filep) != NULL){
         if(strcmp(buffer,"quit\n") == 0){
             break;
         }
 
-        fputs("\n\n<<COMANDOS : ",stdout);
+        fputs("\n\n<executing command: ",stdout);
         fputs(buffer,stdout);
         fputs("\n\n",stdout);
         pipe_get_cmd(buffer);
         fputs("\n\n",stdout);
 
-        printf("\n\nTERMINA>>");
+        printf("\n\ncommand completed>");
 
     }
-    printf("\n\nFIN De ejecucion\n\n");
-    fclose(fp);
+    printf("\n\nexecuting completed\n\n");
+    fclose(filep);
     exit(0);
 }
 
@@ -52,7 +52,7 @@ void execute_file(char *filepath){
 
 int main(int argc, char *argv[]){
     int src = 0;
-    struct stat statbuf;
+    struct stat stat_buf;
 
     if(argc > 2){
         printf("usage: %s [batchFile]\n", argv[0]);
@@ -65,8 +65,8 @@ int main(int argc, char *argv[]){
             return -2;
         }
 
-        stat(argv[1],&statbuf);
-        if(S_ISREG(statbuf.st_mode)){
+        stat(argv[1],&stat_buf);
+        if(S_ISREG(stat_buf.st_mode)){
             execute_file(argv[1]);
         }else{
             printf("%s: File unavailable to open \n",argv[1]);
